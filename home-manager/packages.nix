9fb966a94da0dea604f68fedf39649beb3e284cb
okrag <48834705+okrag@users.noreply.github.com>
2024-01-10 22:32:28 +0100

    Cleanup

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,64 +1,60 @@
 { pkgs, pkgs-master, ... }: {
   home.packages = with pkgs; [
-    mdcat
-    nodePackages.nodemon
-
-    networkmanagerapplet
-    jetbrains.idea-community
-    jdk17
-    brave
-    discord
-    spotify
-    # webcord_patched
-    openboard
-
-    wl-clipboard
-    playerctl
-    gnome.simple-scan
-
-    krita
-
+    # Other
     freshfetch
-    nodePackages.pnpm
-    nodejs-slim_20
-    bun
-    tor-browser-bundle-bin
-    baobab
-    gnome.gnome-system-monitor
-
-    signal-desktop
-    pavucontrol
-
+    mdcat
     qemu
     woeusb-ng
 
+    # Apps
+    networkmanagerapplet
+    brave
+    discord
+    spotify
+    openboard
+    gnome.simple-scan
+    tor-browser-bundle-bin
+    baobab
+    gnome.gnome-system-monitor
+    krita
+    signal-desktop
+    pavucontrol
+
+    # Java
+    jetbrains.idea-community
+    jdk17
+
+    # JS
+    bun
+    nodePackages.pnpm
+    nodejs-slim_20
+    nodePackages.nodemon
+
+    # Gaming
     prismlauncher
     glfw-wayland-minecraft
-
     steam-run
 
     # Rust
     pcmanfm
     rustup
     pkg-config
-    gcc
 
     # C/C++ Language Server and tools
     ccls
     gnumake
+    gcc
 
     # Screenshots
     grim
     slurp
 
-    # Wallpaper
-    swww
-
-    # Nix language formatter
-    nixpkgs-fmt
-
     # Libs
+    nixpkgs-fmt
     openssl_3
     libnotify
+    wl-clipboard
+    swww
+    playerctl
   ];
 }
